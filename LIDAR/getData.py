132ee3e56be18cf0d80ff57b4262c7b445f8f7eb
Luc Anchling <luc.anchling@cpe.fr>
2022-04-01 09:21:43 +0200

    LIDAR - affichage data.txt

--- LIDAR/getData.py
+++ LIDAR/getData.py
@@ -21,36 +21,39 @@ def run(path):
     lidar.disconnect()
     np.save(path, np.array(data))
 
+def analyse(lidar):
+    for i, scan in enumerate(lidar.iter_scans('express')):
+        print('%d: Got %d measures' % (i, len(scan)))
+        data.append([[scan[j][1] for j in range(len(scan))],[scan[j][2] for j in range(len(scan))]])
+        Angle = data[i][0]
+        Distance = data[i][1]
+        X = [Distance[i]*cos(Angle[i]*pi/180) for i in range(len(scan))]
+        Y = [Distance[i]*sin(Angle[i]*pi/180) for i in range(len(scan))]
+        # plt.figure()
+        # plt.scatter(X,Y)
+        # #plt.show()
+        # plt.ion()
+        # plt.draw()
+        # plt.pause(1)
+        # X,Y,Angle,Distance = [],[],[],[]
+        #plt.close()
+
+        if i == 50:
+           break
 
 #run('./examples/data')
 lidar = RPLidar(PORT_NAME)
 data = []
-#try:
-for i, scan in enumerate(lidar.iter_scans('express')):
-    print('%d: Got %d measures' % (i, len(scan)))
-    data.append([[scan[j][1] for j in range(len(scan))],[scan[j][2] for j in range(len(scan))]])
-    Angle = data[i][0]
-    Distance = data[i][1]
-    X = [Distance[i]*cos(Angle[i]*pi/180) for i in range(len(scan))]
-    Y = [Distance[i]*sin(Angle[i]*pi/180) for i in range(len(scan))]
-    # plt.figure()
-    # plt.scatter(X,Y)
-    # #plt.show()
-    # plt.ion()
-    # plt.draw()
-    # plt.pause(1)
-    # X,Y,Angle,Distance = [],[],[],[]
-    #plt.close()
-    
-    if i == 50:
-       break
+try:
+    analyse(lidar)
    
-with open('datalidar.txt','w') as f:
-        f.writelines(["%s\n" % item  for item in data])
-        f.close()
-# except ValueError:
-#     print('Issue : Value Error')
-# except RPLidarException:
-#     print('Issue : RPlidar Error')
-
+# with open('datalidar.txt','w') as f:
+#         f.writelines(["%s\n" % item  for item in data])
+#         f.close()
+except ValueError:
+    print('Issue : Value Error')
+    analyse(lidar)
+except RPLidarException:
+    print('Issue : RPlidar Error')
+    analyse(lidar)
 
